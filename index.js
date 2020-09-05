6501993e91ed24af1dd273201ee58d36cd5de766
Nitin Marale <66687755+nitinmarale@users.noreply.github.com>
2020-09-05 15:33:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var submit =document.getElementById('btn-submit');
+submit.addEventListener('click', function(){
+    alert("Sorry !!!Form is not working ");
+});
+
+
+
+// var backGround = document.getElementById('bg-color');
+// var bgColor = document.getElementById('dark-mode');
+
+
+// backGround.addEventListener('click',function(){
+//   bgColor.classList.toggle('dark-mode')
+
+// })
+
+function makeBold(){
+  document.getElementById('dark-mode').classList.toggle('dark')
+}
+
+
+   
+    
+
+
+  
+
+    
